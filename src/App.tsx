ee70a85ee4b1e1100335264f701241712bd1c9d7
Igor Santos <igorsantos9513@outlook.com>
2021-06-24 22:33:35 -0300

    Aula 2 teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import {Button} from'./components/Buttons';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './Pages/Home';
+import { NewRoom } from "./Pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+
+
 
 function App() {
+
   return (
-    <Button />
+
+    <BrowserRouter>
+    <AuthContextProvider>
+      <Route path='/' exact component={Home}></Route>
+      <Route path='/rooms/new' component={NewRoom}></Route>
+    </AuthContextProvider>
+      
+    </BrowserRouter>
+    
   );
 }
 
